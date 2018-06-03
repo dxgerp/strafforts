6174f5e8e12ee1c6970072a4365028767ea79523
Yi Zeng <iam@yizeng.me>
2018-06-03 17:11:17 +1200

    Update FAQ spec test to check only http_status

--- spec/requests/api/faqs_api_spec.rb
+++ spec/requests/api/faqs_api_spec.rb
@@ -6,13 +6,11 @@ RSpec.describe Api::FaqsController, type: :request do
   it 'GET index should be successful' do
     # arrange.
     url = '/api/faqs/index'
-    expected = "#{expected_folder}#{url}.json"
 
     # act.
     get url
 
     # assert.
     expect(response).to have_http_status(:success)
-    expect(response.body).to eq(File.read(expected))
   end
 end
