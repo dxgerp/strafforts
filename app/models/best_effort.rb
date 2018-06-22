c908655410de331837501d380293d071024c0041
Yi Zeng <iam@yizeng.me>
2018-06-22 11:18:43 +0300

    Remove variable and merge the statement to one line

--- app/models/best_effort.rb
+++ app/models/best_effort.rb
@@ -20,8 +20,7 @@ class BestEffort < ApplicationRecord
 
     results = {}
     items.each do |item|
-      start_date = item.start_date_local.blank? ? item.start_date : item.start_date_local
-      year = start_date.year
+      year = item.start_date_local.blank? ? item.start_date.year : item.start_date_local.year
       if !results[year] || (results[year] && item.elapsed_time < results[year].elapsed_time)
         results[year] = item
       end
