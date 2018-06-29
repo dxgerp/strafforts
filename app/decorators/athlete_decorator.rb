9cd8059427eaeb4df7de7eee1ba837b273fbb02f
Yi Zeng <iam@yizeng.me>
2018-06-29 12:34:08 +0300

    Fix returning_after_inactivity? logic to fix test for #265

--- app/decorators/athlete_decorator.rb
+++ app/decorators/athlete_decorator.rb
@@ -101,7 +101,7 @@ class AthleteDecorator < Draper::Decorator
   end
 
   def returning_after_inactivity?
-    inactivity_days_threshold = ENV['INACTIVITY_DAYS_THRESHOLD'].to_i || 180
+    inactivity_days_threshold = ENV['INACTIVITY_DAYS_THRESHOLD'].blank? ? 180 : ENV['INACTIVITY_DAYS_THRESHOLD'].to_i
     athlete.last_active_at.blank? || athlete.last_active_at.to_date < Date.today - inactivity_days_threshold.days
   end
 
