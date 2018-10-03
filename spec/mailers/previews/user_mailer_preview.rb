1816a41edc6a7cfe0a2fdf5c129e70522702b5c0
Yi Zeng <iam@yizeng.me>
2018-10-03 20:45:43 +1300

    Remove the deprecated Rails mailer files and specs

--- spec/mailers/previews/user_mailer_preview.rb
+++
@@ -1,8 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
-class UserMailerPreview < ActionMailer::Preview
-  # DEPRECATED. This is now handled by Mailer Lite workflow.
-  def welcome_email
-    athlete = Athlete.find_by(id: 9123806)
-    UserMailer.welcome_email(athlete)
-  end
-end
