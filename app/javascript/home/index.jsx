1e6bfd4957c356b251c38e02ef483e81a950bcc6
Yi Zeng <iam@yizeng.me>
2018-10-15 08:58:12 +0800

    Add a IntlProvider Wrapper to manually switch to English when needed
    https://stackoverflow.com/a/51556636/1177636

--- app/javascript/home/index.jsx
+++ app/javascript/home/index.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import config from 'react-global-configuration';
-import { addLocaleData, IntlProvider } from 'react-intl';
-import localeData_en from 'react-intl/locale-data/en';
-import localeData_zh from 'react-intl/locale-data/zh';
 import { Route, Router, Switch } from 'react-router-dom';
 
 import './assets/scss/material-kit-react.css?v=1.3.0';
@@ -11,32 +8,15 @@ import './assets/scss/material-kit-react.css?v=1.3.0';
 import { createBrowserHistory } from 'history';
 
 import configFile from './config';
+import { IntlProvider } from './IntlContext';
 import indexRoutes from './routes/index.jsx';
-import translations_en from './translations/en.json';
-import translations_zh_CN from './translations/zh_CN.json';
 
 var hist = createBrowserHistory();
 
 config.set(configFile);
 
-addLocaleData([...localeData_en, ...localeData_zh]);
-
-const translations = {
-  en: translations_en,
-  zh: translations_zh_CN,
-};
-
-let language = (navigator.languages && navigator.languages[0]) || navigator.language || navigator.userLanguage || 'en';
-const locale = language.split(/[-_]/)[0]; // Language without region code.
-// If there is no translations found for this language,
-// check if the locale is found, otherwise use default 'en'.
-// For example, when 'en-US' doesn't have tranlastions, but 'en' is found, use 'en' instead.
-if (!(language in translations)) {
-  language = locale in translations ? locale : 'en';
-}
-
 ReactDOM.render(
-  <IntlProvider locale={locale} messages={translations[language]}>
+  <IntlProvider>
     <Router history={hist}>
       <Switch>
         {indexRoutes.map((prop, key) => {
--- app/javascript/home/views/Components/HeaderLinks.jsx
+++ app/javascript/home/views/Components/HeaderLinks.jsx
@@ -4,7 +4,10 @@ import { FormattedMessage } from 'react-intl';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import withStyles from '@material-ui/core/styles/withStyles';
+import Tooltip from '@material-ui/core/Tooltip';
+import { GTranslate } from '@material-ui/icons';
 
+import { IntlConsumer } from './../../IntlContext';
 import Button from './material-kit-react/CustomButtons/Button.jsx';
 import headerLinksStyle from './material-kit-react/Styles/components/headerLinksStyle.jsx';
 
@@ -25,6 +28,24 @@ function HeaderLinks({ ...props }) {
         <Button color="transparent" href="#the-team" className={classes.navLink}>
           <FormattedMessage id="home.sectionTeam.title" />
         </Button>
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <IntlConsumer>
+          {({ switchToEnglish }) => (
+            <React.Fragment>
+              <Tooltip
+                id="switch-language-tooltip"
+                title="Switch to English"
+                placement={window.innerWidth > 959 ? 'top' : 'left'}
+                classes={{ tooltip: classes.tooltip }}
+              >
+                <Button color="transparent" onClick={switchToEnglish} className={classes.navLink}>
+                  <GTranslate />
+                </Button>
+              </Tooltip>
+            </React.Fragment>
+          )}
+        </IntlConsumer>
       </ListItem>
     </List>
   );
