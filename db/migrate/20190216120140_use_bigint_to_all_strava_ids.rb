fc81202d1d6802709495c9146aca1b116dc272cf
Yi Zeng <developer@yizeng.me>
2019-03-09 19:28:08 +0100

    athlete.last_activity_retrieved needs to be bigint

--- db/migrate/20190216120140_use_bigint_to_all_strava_ids.rb
+++ db/migrate/20190216120140_use_bigint_to_all_strava_ids.rb
@@ -1,6 +1,7 @@
 class UseBigintToAllStravaIds < ActiveRecord::Migration[5.2]
   def change
     change_column :activities, :id,:bigint
+    change_column :athletes, :last_activity_retrieved,:bigint
     change_column :best_efforts, :activity_id,:bigint
     change_column :races, :activity_id,:bigint
   end
