83bf85853afacadaeccc40b23b5058664bde1fe2
Yi Zeng <developer@yizeng.me>
2019-03-24 16:19:29 +0100

    Update SMTP settings to use ENV variables

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,9 +74,9 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      address:              'smtp.zoho.com',
-      port:                 465,
-      domain:               'strafforts.com',
+      address:              ENV['SMTP_PROVIDER_ADDRESS'],
+      port:                 ENV['SMTP_PROVIDER_PORT'],
+      domain:               ENV['SMTP_PROVIDER_DOMAIN'],
       user_name:            ENV['SMTP_PROVIDER_USERNAME'],
       password:             ENV['SMTP_PROVIDER_PASSWORD'],
       authentication:       'plain',
